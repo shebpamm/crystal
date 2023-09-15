f3b4b28e6c69e5269396c3d0a0d3bbba3dec9a7d
Erik Karsten <erik@opcode.xyz>
2023-09-15 22:10:54 +0300

    tune worker sleep timings

--- src/worker.rs
+++ src/worker.rs
@@ -1,12 +1,22 @@
 use fang::asynk::async_queue::AsyncQueue;
 use fang::asynk::async_worker_pool::AsyncWorkerPool;
+use fang::SleepParams;
+use std::time::Duration;
 use postgres_native_tls::MakeTlsConnector;
 
 pub fn create_worker_pool(
     queue: AsyncQueue<MakeTlsConnector>,
 ) -> AsyncWorkerPool<AsyncQueue<MakeTlsConnector>> {
+    let sleep_params = SleepParams::builder()
+        .sleep_period(Duration::from_secs(1))
+        .min_sleep_period(Duration::from_secs(1))
+        .max_sleep_period(Duration::from_secs(5))
+        .sleep_step(Duration::from_secs(1))
+        .build();
+
     AsyncWorkerPool::builder()
         .number_of_workers(10_u32)
         .queue(queue.clone())
+        .sleep_params(sleep_params)
         .build()
 }
